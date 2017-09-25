52cebf24879a80bb5f38d1badb32593eeaf19518
Andvri <javier.martinez@unet.edu.ve>
2017-09-25 00:41:55 -0400

    mostrando la tabla de salida

---
+++ App/Views/proceso.tem.php
@@ -0,0 +1,12 @@
+<h2>Informacion Enviada Exitosamente</h2>
+<table>
+    <thead>
+        <tr>
+            <th>Dato</th>
+            <th>Valor</th>
+        </tr>
+    </thead>
+    <tbody>
+        [@table]
+    </tbody>
+</table>
