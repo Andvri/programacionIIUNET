9a7df5ef16bedb904fadfdff331d7d6c657bab28
Andvri <javier.martinez@unet.edu.ve>
2017-09-25 00:21:14 -0400

    Mejorando la clase template para ejecutar scripts de php y remplazar tags

--- App/Lib/template.php
+++ App/Lib/template.php
@@ -1,23 +1,57 @@
 <?php
 namespace App\Lib;
 class Template {
-    protected $file;
+    protected $folder;
     protected $values = array();
   
-    public function __construct($file) {
-        $this->file = $file;        
+    public function __construct($folder = null) {
+        if($folder){
+            $this->setFolder( $folder );
+        }     
     }
-    public function set($key, $value) {
-        $this->values[$key] = $value;
+    public function setFolder($folder){
+        $this->folder = $this->folder = rtrim( $folder, '/' );
     }
-      
-    public function output() {
-        if (!file_exists($this->file)) {
-            return "Ocurrio un Error";
+    public function render( $templateNames, $variables = array() ){
+		$template = $this->find_template( $templateNames );
+		$output = '';
+		if ( $template ){
+            $output = $this->render_template( $template, $variables );
+            $output = $this->remplace_tags($output, $variables);
         }
-        $output = file_get_contents($this->file);
+		return $output;
+    }
+
+    private function find_template( $templateNames ){
+		if ( !is_array( $templateNames ) ) {
+			$templateNames = array( $templateNames );
+		}
+		$templateNames = array_reverse( $templateNames );
+		$found = false;
+		foreach( $templateNames as $templateName ){
+			$file = "{$this->folder}/{$templateName}.tem.php";
+			if ( file_exists( $file ) ){
+				$found = $file;
+				break;
+			}
+		}
+		return $found;
+    }
+    
+
+    private function render_template($template, $variables){
+        ob_start();
+        foreach ( $variables as $key => $value) {
+			${$key} = $value;
+        }
+        
+		include $template;
+		return ob_get_clean();
+    }
       
-        foreach ($this->values as $key => $value) {
+    private function remplace_tags($output, $variables) {
+      
+        foreach ($variables as $key => $value) {
             $tagToReplace = "[@$key]";
             $output = str_replace($tagToReplace, $value, $output);
         }
